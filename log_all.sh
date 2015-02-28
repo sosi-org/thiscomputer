f06f17a59766043b35a4c4c54e696c39b44009f0
Sohail Si <sohale@gmail.com>
2015-02-28 18:28:50 +0000

    minor bug

--- log_all.sh
+++ log_all.sh
@@ -3,4 +3,4 @@ cd $FLDR
 ./log_heat.sh
 ./log_battery.sh
 
-~/cs/thiscomputer/graph1 --th 24  ~/cs/thiscomputer/battery_values.log 100 ~/cs/thiscomputer/heat_values.log
+~/cs/thiscomputer/graph1 --th 24 ~/cs/thiscomputer/battery_values.log 100 ~/cs/thiscomputer/heat_values.log
